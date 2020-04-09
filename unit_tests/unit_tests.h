886a3fc9551b304e6f76f1c77731ff6167417e96
alex-zinin <43748062+alex-zinin@users.noreply.github.com>
2020-04-09 17:41:50 +0300

    Create unit_tests.h

---
+++ unit_tests/unit_tests.h
@@ -0,0 +1,8 @@
+#include "test_runner.h"
+#include"../vector2/vector2.h"
+#include"../link/link.h"
+#include"../Game_World/Game_World.h"
+
+void Test_Vector2();
+void Test_Sender();
+void Test_Changer_From_Message();
