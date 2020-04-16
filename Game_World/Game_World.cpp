95080f83ca68f8fdbf701093bb66429e559e5b01
alex-zinin <43748062+alex-zinin@users.noreply.github.com>
2020-04-16 17:32:44 +0300

    Update Game_World.cpp

--- Game_World/Game_World.cpp
+++ Game_World/Game_World.cpp
@@ -2,32 +2,62 @@
 
 
 
-Game_World::Game_World(int x_size, int y_size): player(400, 200), player2(-10, -10), world_x_size(x_size), world_y_size(y_size){
+Game_World::Game_World(int x_size, int y_size): player(350, 700), player2(-200, -200), world_x_size(x_size), world_y_size(y_size){
 
     for(int i = 0; i < GAME_WORLD_SIZE/100; i++)
     {
-        entities.push_back(std::make_shared<Wall>(i*100, 0));
-        entities.push_back(std::make_shared<Wall>(0, i*100));
-        entities.push_back(std::make_shared<Wall>(GAME_WORLD_SIZE-1, i*100));
-        entities.push_back(std::make_shared<Wall>(i*100, GAME_WORLD_SIZE-1));
+        entities.push_back(std::make_shared<Wall>(i*100 + 200, 200));
+        entities.push_back(std::make_shared<Wall>(200, i*100));
+        entities.push_back(std::make_shared<Wall>(GAME_WORLD_SIZE-1 - 200, i*100));
+        entities.push_back(std::make_shared<Wall>(i*100, GAME_WORLD_SIZE-1 - 200));
+        entities.push_back(std::make_shared<Tree1>(i*100, 0));
+        entities.push_back(std::make_shared<Tree2>(0, i*100));
+        entities.push_back(std::make_shared<Tree2>(GAME_WORLD_SIZE-1, i*100));
+        entities.push_back(std::make_shared<Tree1>(i*100, GAME_WORLD_SIZE-1 - 100));
     }
-    entities.push_back(std::make_shared<Fire_Shooter>(737, 945));
-    //std::cout << entities.back()->health << std::endl;
-    entities.push_back(std::make_shared<Hospital_House>(700, 700));
-    //std::cout << entities.back()->health << std::endl;
+
+    entities.push_back(std::make_shared<House>(1600, 800));
+    entities.push_back(std::make_shared<Bon_Fire>(1800, 900));
+     entities.push_back(std::make_shared<Pit>(1500, 900));
+
+     entities.push_back(std::make_shared<Torch>(900, 900));
+    entities.push_back(std::make_shared<Fire_Shooter>(1200, 700));
+    entities.push_back(std::make_shared<Hospital_House>(900, 700));
     entities.push_back(std::make_shared<Witch>(1000, 500, 2, 2));
 
-    entities.push_back(std::make_shared<Hospital_House>(1500, 700));
 
-    entities.push_back(std::make_shared<Wall>(500, 500));
-    //std::cout << entities.back()->health << std::endl;
-    std::vector<std::pair<int, int>> zombie_positions = {{1200, 200}, {1001, 300}, {1001, 234}, {701, 235}, {1003, 725}, {1325, 462}, {1322, 435}, {1345, 127}};
+
+    entities.push_back(std::make_shared<Hospital_House>(2200, 1700));
+    entities.push_back(std::make_shared<Bon_Fire>(2000, 1800));
+    entities.push_back(std::make_shared<Bench>(2400, 1800));
+
+
+    entities.push_back(std::make_shared<Bon_Fire>(1000, 1400));
+    entities.push_back(std::make_shared<Basin>(700, 1400));
+
+
+    entities.push_back(std::make_shared<Bon_Fire>(1000, 1000));
+    entities.push_back(std::make_shared<Cart>(700, 1000));
+
+
+    std::vector<std::pair<int, int>> tree1_positions = {{1200, 2200}, {1201, 1300}, {1701, 1234}, {2701, 1735}, {1003, 2225}, {1325, 2462}, {1322, 1435}, {1345, 2127}};
+    for(auto &x: tree1_positions)
+        entities.push_back(std::make_shared<Tree1>(x.first, x.second));
+
+
+    std::vector<std::pair<int, int>> tree2_positions = {{1900, 2109}, {2201, 2100}, {2501, 2234}, {2601, 2535}, {1403, 2125}, {2325, 2362}, {2322, 2435}, {2345, 2127}};
+    for(auto &x: tree2_positions)
+        entities.push_back(std::make_shared<Tree2>(x.first, x.second));
+
+    std::vector<std::pair<int, int>> tree3_positions = {{900, 1109}, {201, 2140}, {701, 234}, {2601, 635}, {2403, 512}, {2325, 762}, {2322, 435}, {2345, 727}};
+    for(auto &x: tree3_positions)
+        entities.push_back(std::make_shared<Tree3>(x.first, x.second));
+
+    std::vector<std::pair<int, int>> zombie_positions = {{1200, 2100}, {1001, 2300}, {1001, 2234}, {1701, 2235}, {1003, 2325}, {1325, 1462}, {1322, 1435}, {1345, 1127}};
     for(auto &x: zombie_positions)
         entities.push_back(std::make_shared<Zombie>(x.first, x.second, 2, 2));
 
 
-    entities.push_back(std::make_shared<Wall>(999, 1010));
-    entities.push_back(std::make_shared<Bon_Fire>(700, 500));
 
     world.setPosition(0, 0);
     world.setScale(1.0f, 1.0f);
